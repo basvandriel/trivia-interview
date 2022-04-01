a9bf9f718cc0ec37556aac11948ebd7123bb6156
Bas <basvandriel94@gmail.com>
2022-04-01 13:18:21 +0200

    Testing setup with in memory db

---
+++ src/test/java/com/bas/trivia/QuestionControllerTests.java
@@ -0,0 +1,26 @@
+package com.bas.trivia;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class QuestionControllerTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private AnswerRepository repository;
+    
+    @Test
+    void testSomething() {
+        assertThat(true);
+    }
+}
