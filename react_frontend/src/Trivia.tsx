fe5a70cbbcf5a69be9a6af9c143115ad4c61aacf
Bas van Driel <bas.vandriel.nl@gmail.com>
2022-04-04 13:38:40 +0200

    Move funcs in hooks

--- react_frontend/src/Trivia.tsx
+++ react_frontend/src/Trivia.tsx
@@ -1,13 +1,20 @@
 import { useEffect, useRef, useState } from "react";
 import Question from "./Question";
-
-/**
- * Move into env
- */
-const API_URL = "http://localhost:8080";
+import useQuestionsRequest from "./hooks/useQuestionsQuery";
+import useCheckAnswerRequest from "./hooks/useCheckAnswerRequest";
 
 export const Trivia = () => {
   const [questions, setQuestions] = useState<Question[]>([]);
+
+  /**
+   * To fetch the questions
+   */
+  const fetchQuestions = useQuestionsRequest();
+
+  /**
+   * To check the answer
+   */
+  const check = useCheckAnswerRequest();
 
   /**
    * Prevent StrictMode from rendering this component twice
@@ -20,30 +27,21 @@ export const Trivia = () => {
   useEffect(() => {
     if (renderRef.current === true) return;
 
-    const fetchQuestions = async () => {
-      const response = await fetch(`${API_URL}/questions`);
-      const data = await response.json();
+    (async () => {
+      const response = await fetchQuestions();
+      const json = await response.json();
 
-      setQuestions(data);
-    };
-
-    fetchQuestions();
+      setQuestions(json);
+    })();
     renderRef.current = true;
-  }, []);
+  }, [fetchQuestions]);
 
   if (questions.length === 0) return <p>Loading.</p>;
 
   const submit = async (answer: string) => {
     const question: string = questions[0].question;
+    const response = await check(question, answer);
 
-    const response = await fetch(`${API_URL}/checkanswer`, {
-      method: "POST",
-      body: JSON.stringify({
-        question,
-        answer,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
     // If the answer was correct
     const ok: boolean = await response.json();
 
@@ -56,7 +54,12 @@ export const Trivia = () => {
       <div>
         {questions[0].possible_answers.map((v, i) => (
           <label key={i} htmlFor={`answer_${i}`}>
-            <input type="radio" id={`answer_${i}`} onChange={() => submit(v)} />
+            <input
+              type="radio"
+              id={`answer_${i}`}
+              name="possible_answer"
+              onChange={() => submit(v)}
+            />
             <span dangerouslySetInnerHTML={{ __html: v }} />
           </label>
         ))}
