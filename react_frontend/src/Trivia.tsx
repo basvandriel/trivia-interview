f00d95c9df4fe034fe2cfa452f741ca57c5868cc
Bas van Driel <bas.vandriel.nl@gmail.com>
2022-04-04 13:23:14 +0200

    Only render once

--- react_frontend/src/Trivia.tsx
+++ react_frontend/src/Trivia.tsx
@@ -1,54 +1,66 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Question from "./Question";
 
 /**
  * Move into env
  */
- const API_URL = 'http://localhost:8080'
+const API_URL = "http://localhost:8080";
 
 export const Trivia = () => {
-    const [questions, setQuestions] = useState<Question[]>([]);
+  const [questions, setQuestions] = useState<Question[]>([]);
 
-    
-    useEffect(() => {
-        const fetchQuestions = async () => {
-          const response = await fetch(`${API_URL}/questions`);  
-          const data = await response.json();
-    
-          setQuestions(data)
-        }
-        fetchQuestions();
-      }, [])
-    
-      if(questions.length === 0) return <p>Loading.</p>
+  /**
+   * Prevent StrictMode from rendering this component twice
+   */
+  const renderRef = useRef(false);
 
-      const submit = async (answer: string) => {
-        const question: string = questions[0].question
-        console.log("Submitted answer "+ answer + "to question " + question)
-    
-        const response = await fetch(`${API_URL}/checkanswer`, {
-          method: 'POST',
-          body: JSON.stringify({
-            question, answer
-          }),
-          headers: { 'Content-Type': 'application/json' },
-        })
-    
-        // If the answer was correct0
-        const ok: boolean = await response.json();
-    
-        console.log(ok)
-      }
-    
-    return <>
-        <p dangerouslySetInnerHTML={{ __html: questions[0].question}}/>
-        <div>
-            {questions[0].possible_answers.map((v, i) => (
-            <label key={i} htmlFor={`answer_${i}`}>
-                <input type={'radio'} name='possible_answer' id={`answer_${i}`} onChange={(e) => submit(v)} />
-                {v.normalize()}
-            </label>
-            ))}
-        </div>
+  /**
+   * Load the questions on page load
+   */
+  useEffect(() => {
+    if (renderRef.current === true) return;
+
+    const fetchQuestions = async () => {
+      const response = await fetch(`${API_URL}/questions`);
+      const data = await response.json();
+
+      setQuestions(data);
+    };
+
+    fetchQuestions();
+    renderRef.current = true;
+  }, []);
+
+  if (questions.length === 0) return <p>Loading.</p>;
+
+  const submit = async (answer: string) => {
+    const question: string = questions[0].question;
+
+    const response = await fetch(`${API_URL}/checkanswer`, {
+      method: "POST",
+      body: JSON.stringify({
+        question,
+        answer,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    // If the answer was correct
+    const ok: boolean = await response.json();
+
+    console.log(ok);
+  };
+
+  return (
+    <>
+      <p dangerouslySetInnerHTML={{ __html: questions[0].question }} />
+      <div>
+        {questions[0].possible_answers.map((v, i) => (
+          <label key={i} htmlFor={`answer_${i}`}>
+            <input type="radio" id={`answer_${i}`} onChange={() => submit(v)} />
+            <span dangerouslySetInnerHTML={{ __html: v }} />
+          </label>
+        ))}
+      </div>
     </>
-}
+  );
+};
